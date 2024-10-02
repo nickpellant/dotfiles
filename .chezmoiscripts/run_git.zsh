a72b75ac762d7e23eb9c86eda93573436051ea1d
Nick Pellant <nick@nickpellant.com>
2024-10-02 10:39:58 +0700

    Update shell setup script

--- .chezmoiscripts/run_git.zsh
+++ .chezmoiscripts/run_git.zsh
@@ -4,5 +4,5 @@ source "$(chezmoi source-path)/.chezmoihelpers/gum_helpers.zsh"
 
 gum_header "Git"
 
-gum_spin "Updating git workspaces..." "git" "git workspace update" "Git workspaces updated."
-gum_spin "Archiving git repositories..." "git" "git workspace archive" "Git repositories archived."
+gum_spin "Updating git workspaces..." "git-workspace" "git workspace update" "Git workspaces updated."
+gum_spin "Archiving git repositories..." "git-workspace" "git workspace archive" "Git repositories archived."
