b781e8589ec8bc40256ed3a9146d3cf03cfe815b
Nick Pellant <nick@capimoney.com>
2025-03-12 20:47:57 +0100

    Remove obsolete configs and template files

--- .chezmoiscripts/run_git.zsh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-source "$(chezmoi source-path)/.chezmoihelpers/gum_helpers.zsh"
-
-gum_header "Git"
-
-gum_spin "Updating git workspaces..." "git-workspace" "git workspace update" "Git workspaces updated."
-gum_spin "Archiving git repositories..." "git-workspace" "git workspace archive" "Git repositories archived."
